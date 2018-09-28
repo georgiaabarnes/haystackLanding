13e4a0635bb71ce355b3b2724ee76e3ced5ddfde
Dustin Leggans <dustin@osatechnologypartners.com>
2018-09-27 23:21:59 -0400

    removed contact form and tiers

---
+++ _hubspot_form.js
@@ -0,0 +1,32 @@
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+<div class="form-wrapper">
+  <div class="hbspt-form">
+    <!--[if lte IE 8]>
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+    <![endif]-->
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
+    <script>
+    hbspt.forms.create({
+      css: 'display:none;',
+      portalId: '4624875',
+      formId: '9bcf0851-cb64-44dc-a5da-fbf1ca36e3c4',
+      target: '.hbspt-form',
+      onFormReady: function($form) {
+        $form.attr('target', 'hubspot-iframe');
+      },
+      onFormSubmit: function($form) {
+        jQuery('.form-wrapper .hbspt-form').hide();
+        jQuery('.form-confirmation').show();
+      }
+    });
+    </script>
+  </div>
+
+
+  <iframe class="hidden" name="hubspot-iframe" id="hubspot-iframe"></iframe>
+  <div class="form-confirmation">
+    <div class="form-confirmation-inner">
+      <!-- custom form flip here -->
+    </div>
+  </div>
+</div>
